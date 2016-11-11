32edc98e3c01c6584902bafcfe036456e325db2a
rafuck <rfkadyrov@gmail.com>
2016-11-11 15:44:47 +0300

    thread pool temptlate

--- main_template.cpp
+++ main_template.cpp
@@ -67,6 +67,97 @@ void qSortSimpleParallel(const F &comparator, T *a, int left, int right){
 	}
 }
 
+template<typename F, typename T>
+class QSortThreadPool{
+private:
+	std::thread *threadPool;
+	T *a;
+	int N;
+	const F &comparator;
+
+	IIQueue<std::packaged_task<void()>> taskQueue;
+	std::function<void(int, int)> sortMethod;
+	
+	// May be some code here ------- ???? ---------
+
+	void threadFunction(){
+		while(true){
+			auto task = taskQueue.wait();
+			if (!task.valid()){
+				break;
+			}
+
+			task();
+			
+			// May some code here -------- ???? -------
+		}
+	}
+
+	void _qSort(int left, int right){
+		int l = left,
+			r = right;
+		T b = a[left + (right-left)/2];
+		while(l<=r){
+			while(comparator(a[l], b) < 0) l++;
+			while(comparator(a[r], b) > 0) r--;
+
+			if (l <= r){
+				std::swap(a[l], a[r]);
+				l++; r--;
+			}
+		}
+
+		if (l < right){
+			if (left < r && right - l > 256){
+				// Create task -------- ???? -------
+			}
+			else{
+				_qSort(l, right);
+			}
+		}
+
+		if (left < r ){
+			_qSort(left, r );
+		}
+	}
+public:
+	QSortThreadPool(const F &_comparator):comparator(_comparator){
+		sortMethod = std::bind(&QSortThreadPool<F,T>::_qSort, this, std::placeholders::_1, std::placeholders::_2);
+
+		const size_t nCores = std::thread::hardware_concurrency();
+		threadPool = new std::thread[nCores];
+		for(int i=0; i<nCores; ++i){
+			threadPool[i] = std::thread(std::bind(&QSortThreadPool<F,T>::threadFunction, this));
+		}
+	}
+
+	void operator()(T *_a, int _N){
+		a = _a;
+		N = _N;
+
+		_qSort(0, N-1);
+
+		
+		// May be some code here ------ ???????? --------
+	}
+
+	~QSortThreadPool(){
+		const size_t nCores = std::thread::hardware_concurrency();
+		for(int i=0; i<nCores; ++i){
+			std::packaged_task<void()> task;
+			taskQueue.push(std::move(task));
+		}
+
+		for(int i=0; i<nCores; ++i){
+			if (threadPool[i].joinable()){
+				threadPool[i].join();
+			}
+		}
+
+		delete[] threadPool;
+	}
+};
+
 void print_a(const char *prefix, const double *a, int N, int tail = 0){
 	printf("\n%s:\n", prefix);
 	if (tail < 1 || tail > N/2){
@@ -126,6 +217,13 @@ int main(int argc, char *argv[]){
 	timer();
 	qSortSimpleParallel(comparator<double>, a, 0, N-1);
 	printf("\nSimple parallel sorted at %1.9lf seconds", timer());
+	print_a("Sorted(A)", a, N);
+	
+	randomize(a, N);
+	QSortThreadPool<int(const double&, const double&), double> sorter(comparator<double>);
+	timer();
+	sorter(a, N);
+	printf("\nThread pool parallel sorted at %2.9lf seconds", timer());
 	print_a("Sorted(A)", a, N);
 
 	delete[] a;
