94dd30c50839a8ec7ff85c8e80d240b6abde0e0b
rafuck <rfkadyrov@gmail.com>
2016-12-09 16:20:36 +0300

    ...

--- main_template.cpp
+++ main_template.cpp
@@ -1,12 +1,13 @@
-#ifdef _MSC_VER
+#ifdef _MSC_VER || _WIN32 || _WIN64
 	#include "stdafx.h"
 #endif
-#include <atomic>
+
 #include <cstdlib>
 #include <cstdio>
 #include <thread>
 #include <algorithm>
 #include <time.h>
+#include <atomic>
 #include <random>
 #include <future>
 #include <functional>
@@ -38,7 +39,7 @@ void qSort(const F &comparator, T *a, int left, int right){
 }
 
 template<typename F, typename T>
-void qSortSimpleParallel(F comparator, T *a, int left, int right){
+void qSortSimpleParallel(const F comparator, T *a, int left, int right){
 	int l = left,
 		r = right;
 	T b = a[left + (right-left)/2];
@@ -72,7 +73,8 @@ void qSortSimpleParallel(F comparator, T *a, int left, int right){
 	}
 }
 
-template<typename F, typename T>
+
+template<typename F, typename T, typename Q>
 class QSortThreadPool{
 private:
 	std::thread *threadPool;
@@ -80,10 +82,13 @@ private:
 	int N;
 	const F &comparator;
 
-	IIQueue<std::packaged_task<int()>> taskQueue;
+	std::atomic<int> taskCounter;
 	std::function<int(int, int)> sortMethod;
-	
-	// May be some code here ------- ???? ---------
+
+	std::condition_variable end;
+	std::mutex endLock;
+
+	Q taskQueue;
 
 	void threadFunction(){
 		while(true){
@@ -93,8 +98,11 @@ private:
 			}
 
 			task();
-			
-			// May some code here -------- ???? -------
+			int old = taskCounter.fetch_add(-1, std::memory_order_relaxed);
+			if (old == 1){
+				std::lock_guard<std::mutex> lk(endLock);
+				end.notify_one();
+			}
 		}
 	}
 
@@ -114,7 +122,13 @@ private:
 
 		if (l < right){
 			if (left < r && right - l > 256){
-				// Create task -------- ???? -------
+				taskCounter.fetch_add(1, std::memory_order_relaxed);
+				auto task = std::packaged_task<int()>(
+					std::bind(
+						&QSortThreadPool::_qSort, this, l, right
+					)
+				);
+				taskQueue.push(std::move(task));
 			}
 			else{
 				_qSort(l, right);
@@ -124,28 +138,41 @@ private:
 		if (left < r ){
 			_qSort(left, r );
 		}
-		
+
 		return 0;
 	}
 public:
 	QSortThreadPool(const F &_comparator):comparator(_comparator){
-		sortMethod = std::bind(&QSortThreadPool<F,T>::_qSort, this, std::placeholders::_1, std::placeholders::_2);
-
+		sortMethod = std::bind(&QSortThreadPool<F,T,Q>::_qSort, this, std::placeholders::_1, std::placeholders::_2);
 		const size_t nCores = std::thread::hardware_concurrency();
 		threadPool = new std::thread[nCores];
 		for(int i=0; i<nCores; ++i){
-			threadPool[i] = std::thread(std::bind(&QSortThreadPool<F,T>::threadFunction, this));
+			threadPool[i] = std::thread(std::bind(&QSortThreadPool<F,T,Q>::threadFunction, this));
 		}
 	}
 
 	void operator()(T *_a, int _N){
 		a = _a;
 		N = _N;
+		taskCounter = 0;
 
 		_qSort(0, N-1);
 
 		
-		// May be some code here ------ ???????? --------
+		if (taskCounter){
+			endLock.lock();
+			if (taskCounter){
+				endLock.unlock();
+
+				std::unique_lock<std::mutex> lk(endLock);
+				end.wait(lk, [this]{
+					return taskCounter.load(std::memory_order_relaxed) == 0;
+				});
+			}
+			else{
+				endLock.unlock();
+			}
+		}
 	}
 
 	~QSortThreadPool(){
@@ -165,11 +192,9 @@ public:
 	}
 };
 
-void print_a(const char *prefix, const double *a, int N, int tail = 0){
+void print_a(const char *prefix, const double *a, int N){
 	printf("\n%s:\n", prefix);
-	if (tail < 1 || tail > N/2){
-		tail = N/2;
-	}
+	int tail = N/2;
 	if (tail > 10){
 		tail = 10;
 	}
@@ -203,7 +228,7 @@ double timer(){
 
 void randomize(double *a, int N){
 	std::random_device rd;
-    	std::srand(rd());
+    std::srand(rd());
 
 	for(int i=0; i<N; ++i){
 		a[i] = (double)(std::rand())/RAND_MAX;
@@ -225,16 +250,25 @@ int main(int argc, char *argv[]){
 	qSortSimpleParallel(comparator<double>, a, 0, N-1);
 	printf("\nSimple parallel sorted at %1.9lf seconds", timer());
 	print_a("Sorted(A)", a, N);*/
-	
-	randomize(a, N);
-	QSortThreadPool<int(const double&, const double&), double> sorter(comparator<double>);
-	timer();
-	sorter(a, N);
-	printf("\nThread pool parallel sorted at %2.9lf seconds", timer());
-	print_a("Sorted(A)", a, N);
+
+	{
+		randomize(a, N);
+		QSortThreadPool<int(const double&, const double&), double, IIQueue<std::packaged_task<int()>>> sorter(comparator<double>);
+		timer();
+		sorter(a, N);
+		printf("\nAsync std cond parallel sorted at %2.9lf seconds", timer());
+		print_a("Sorted(A)", a, N);
+	}
+
+	{
+		randomize(a, N);
+		QSortThreadPool<int(const double&, const double&), double, IIQueueMutex<std::packaged_task<int()>>> sorter(comparator<double>);
+		timer();
+		sorter(a, N);
+		printf("\nAsync cond mutex parallel sorted at %2.9lf seconds", timer());
+		print_a("Sorted(A)", a, N);
+	}
 
 	delete[] a;
-	
-	getchar();
 	return EXIT_SUCCESS;
 }
